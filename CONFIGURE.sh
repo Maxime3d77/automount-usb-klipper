3a76a67e6f05d7a9e9513a44c537930b3bd4e861
Maxime3d77 <114477279+Maxime3d77@users.noreply.github.com>
2024-11-07 09:31:04 +0100

    update usb tool

--- CONFIGURE.sh
+++ CONFIGURE.sh
@@ -13,9 +13,14 @@ cp ./usb-mount@.service /etc/systemd/system/usb-mount@.service
 
 # Create udev rule to start/stop usb-mount@.service on hotplug/unplug
 cat ./99-local.rules.usb-mount >> /etc/udev/rules.d/99-local.rules
+sudo chmod +x /usr/local/bin/usb-mount.sh
+sudo chmod 644 /etc/systemd/system/usb-mount@.service
+sudo chmod 644 /etc/udev/rules.d/99-local.rules
 
-systemctl daemon-reload
-udevadm control --reload-rules
+
+sudo systemctl daemon-reload
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 # Create folder mount point for klipper
 if [ ! -d "$MOUNT_POINT" ]; then
@@ -23,6 +28,8 @@ if [ ! -d "$MOUNT_POINT" ]; then
     if [ $? -eq 0 ]; then
         echo "$(date): Dossier $MOUNT_POINT créé" >> "$LOG_FILE"
         chmod 777 "$MOUNT_POINT"
+        sudo chown pi:pi "$MOUNT_POINT"
+
     else
         echo "$(date): Erreur lors de la création du dossier $MOUNT_POINT" >> "$LOG_FILE"
         exit 1
--- usb-mount.sh
+++ usb-mount.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
-# If you are executing this script in cron with a restricted environment,
-# modify the shebang to specify appropriate path; /bin/bash in most distros.
-# And, also if you aren't comfortable using(abuse?) env command.
-
 # This script is based on https://serverfault.com/a/767079 posted
 # by Mike Blackwell, modified to our needs. Credits to the author.
 
-# This script is called from systemd unit file to mount or unmount
-# a USB drive.
-
 PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
-log="logger -t usb-mount.sh -s "
+log="logger -t usb-mount.sh -s"
 
-usage()
-{
-    ${log} "Usage: $0 {add|remove} device_name (e.g. sdb1)"
+usage() {
+    ${log} "Usage: $0 {add|remove} device_name (e.g. sda1)"
     exit 1
 }
 
@@ -26,73 +18,45 @@ ACTION=$1
 DEVBASE=$2
 DEVICE="/dev/${DEVBASE}"
 
-# See if this drive is already mounted, and if so where
-MOUNT_POINT=$(mount | grep ${DEVICE} | awk '{ print $3 }')
+# Get the mount point based on the device name
+MOUNT_POINT="/home/pi/printer_data/gcodes/USB"
 
-DEV_LABEL=""
-
-do_mount()
-{
-    if [[ -n ${MOUNT_POINT} ]]; then
-        ${log} "Warning: ${DEVICE} is already mounted at ${MOUNT_POINT}"
+do_mount() {
+    if mount | grep -q "${DEVICE}"; then
+        ${log} "Warning: ${DEVICE} is already mounted"
         exit 1
     fi
 
-    # Get info for this drive: $ID_FS_LABEL and $ID_FS_TYPE
-    eval $(blkid -o udev ${DEVICE} | grep -i -e "ID_FS_LABEL" -e "ID_FS_TYPE")
-
-    # Figure out a mount point to use
-    LABEL=${ID_FS_LABEL}
-    if grep -q " /media/${LABEL} " /etc/mtab; then
-        # Already in use, make a unique one
-        LABEL+="-${DEVBASE}"
-    fi
-    DEV_LABEL="${LABEL}"
-
-    # Use the device name in case the drive doesn't have label
-    if [ -z ${DEV_LABEL} ]; then
-        DEV_LABEL="${DEVBASE}"
-    fi
-
-    #MOUNT_POINT="/media/${DEV_LABEL}"
-    MOUNT_POINT="/home/pi/printer_data/gcodes/USB/"
-
-    ${log} "Mount point: ${MOUNT_POINT}"
-
+    # Create a mount point if necessary
     mkdir -p ${MOUNT_POINT}
 
-    # Global mount options
+    # Mount options (for vfat filesystems, adjust as needed for others)
     OPTS="rw,relatime"
-
-    # File system type specific mount options
+    eval $(blkid -o udev ${DEVICE} | grep -i -e "ID_FS_LABEL" -e "ID_FS_TYPE")
     if [[ ${ID_FS_TYPE} == "vfat" ]]; then
         OPTS+=",users,gid=100,umask=000,shortname=mixed,utf8=1,flush"
     fi
 
+    # Mount the device
     if ! mount -o ${OPTS} ${DEVICE} ${MOUNT_POINT}; then
         ${log} "Error mounting ${DEVICE} (status = $?)"
         rmdir "${MOUNT_POINT}"
         exit 1
     else
-        # Track the mounted drives
-        echo "${MOUNT_POINT}:${DEVBASE}" | cat >> "/var/log/usb-mount.track" 
+        echo "${MOUNT_POINT}:${DEVBASE}" | cat >> "/var/log/usb-mount.track"
+        ${log} "Mounted ${DEVICE} at ${MOUNT_POINT}"
     fi
-
-    ${log} "Mounted ${DEVICE} at ${MOUNT_POINT}"
 }
 
-do_unmount()
-{
-    if [[ -z ${MOUNT_POINT} ]]; then
-        ${log} "Warning: ${DEVICE} is not mounted"
-    else
-        umount -l ${DEVICE}
-	${log} "Unmounted ${DEVICE} from ${MOUNT_POINT}"
-        /bin/rmdir "${MOUNT_POINT}"
+do_unmount() {
+    if mount | grep -q "${DEVICE}"; then
+        umount ${DEVICE}
+        ${log} "Unmounted ${DEVICE} from ${MOUNT_POINT}"
+        /bin/rmdir ${MOUNT_POINT}
         sed -i.bak "\@${MOUNT_POINT}@d" /var/log/usb-mount.track
+    else
+        ${log} "Warning: ${DEVICE} is not mounted"
     fi
-
-
 }
 
 case "${ACTION}" in
